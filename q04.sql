542577f44f32117e656d6d92d8ea649b83e14ee0
Juan Esteban Gonzalez Murcia <50414971+juegonzalezmu@users.noreply.github.com>
2019-08-20 20:59:03 -0500

    Update q04.sql

--- q04.sql
+++ q04.sql
@@ -3,5 +3,5 @@
 ## para los registros de la tabla tbl1 para los que la 
 ## columna c16 empieza por la misma letra de la columna K0
 ## 
-
+SELECT K0, c16 FROM tb11 WHERE substr(c16,1,1)=substr(K0,1,1);
 
