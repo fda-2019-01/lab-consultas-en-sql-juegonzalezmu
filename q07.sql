242d2acf08c60661bb6a2f92d756a1e0b2d07001
Juan Esteban Gonzalez Murcia <50414971+juegonzalezmu@users.noreply.github.com>
2019-08-20 20:46:55 -0500

    Update q07.sql

--- q07.sql
+++ q07.sql
@@ -4,5 +4,5 @@
 ## registros con K0 diferente a A y B y c13 diferente 
 ## de 200 y 900
 ##
-
+SELECT * FROM tb11 WHERE K0 NOT LIKE 'A%' and K0 NOT LIKE 'B%' and c13 NOT IN ('200','900') ORDER BY c14;
 
